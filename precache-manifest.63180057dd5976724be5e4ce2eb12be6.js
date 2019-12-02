b5fd6fff6707fcff71b5bb248f1e8a771a6d8835
Daniel Hoyos <danielhoyos350@gmail.com>
2019-12-02 09:35:32 -0500

    Updates

---
+++ precache-manifest.63180057dd5976724be5e4ce2eb12be6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ca3f48a3af8c8da4854b3d96500cfca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ffb251a05a694d52efe",
+    "url": "/static/css/2.d4c5f937.chunk.css"
+  },
+  {
+    "revision": "8fdc30678f5e08d067ba",
+    "url": "/static/css/main.0d2b07fc.chunk.css"
+  },
+  {
+    "revision": "7ffb251a05a694d52efe",
+    "url": "/static/js/2.c04ba93f.chunk.js"
+  },
+  {
+    "revision": "8fdc30678f5e08d067ba",
+    "url": "/static/js/main.bc862778.chunk.js"
+  },
+  {
+    "revision": "1e9ae0539e3103f7c27d",
+    "url": "/static/js/runtime-main.973c6ea2.js"
+  },
+  {
+    "revision": "4de9545a0bf1fb83f91eeafd2d0823c4",
+    "url": "/static/media/Informe.4de9545a.pdf"
+  },
+  {
+    "revision": "4349942df503d2d04c6099e3c02a3c06",
+    "url": "/static/media/bg_contacto_info.4349942d.png"
+  },
+  {
+    "revision": "fcb3442eefe86993802b071b489b028e",
+    "url": "/static/media/logo.fcb3442e.png"
+  },
+  {
+    "revision": "5a9681c876cc158bf5e71eaa13bc162e",
+    "url": "/static/media/pdf_logo.5a9681c8.svg"
+  },
+  {
+    "revision": "64542218f1adb068b87032b62418a07f",
+    "url": "/static/media/slide1.64542218.jpg"
+  },
+  {
+    "revision": "0e217c265ea600d0a6f2ebdae35d99aa",
+    "url": "/static/media/slide2.0e217c26.jpg"
+  },
+  {
+    "revision": "2f6a7a63d86cb6ad0587425c1839b3de",
+    "url": "/static/media/slide3.2f6a7a63.jpg"
+  },
+  {
+    "revision": "25634f8243d8c12b6c53454868fa3bda",
+    "url": "/static/media/slide4.25634f82.jpg"
+  },
+  {
+    "revision": "5cbfe3d300027c8b3fefb971d634d240",
+    "url": "/static/media/slide5.5cbfe3d3.jpg"
+  }
+]);
